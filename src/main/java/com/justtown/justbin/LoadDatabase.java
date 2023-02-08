16f64cb91c9f0858ab6de77f8a3255438488693a
Vlad Glazunov <v.glazunov@global-system.ru>
2023-02-08 22:17:43 +0300

    feat: add database bean

---
+++ src/main/java/com/justtown/justbin/LoadDatabase.java
@@ -0,0 +1,21 @@
+package com.justtown.justbin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(MessageRepository repository) {
+
+        return args -> {
+            
+        };
+    }
+
+}
