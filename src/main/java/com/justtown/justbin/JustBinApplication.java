e9023cb0dd9c91c8dac5dfde79bbd7f208f5206a
Vlad Glazunov <v.glazunov@global-system.ru>
2023-02-08 22:09:36 +0300

    init commit

---
+++ src/main/java/com/justtown/justbin/JustBinApplication.java
@@ -0,0 +1,13 @@
+package com.justtown.justbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justtown/justbin/JustBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justtown.justbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
